ea6c97c9f4aee012c5e03b0d9bf3b4d2165ded5b
IvanVideo <ramazanov.iv@gmail.com>
2022-01-08 17:40:05 +0300

    fix: исправил опечатку

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ function Header() {
 
     return (
         <div>
-            <h1 className='header__title'>Работа с таблицой</h1>
+            <h1 className='header__title'>Работа с таблицей</h1>
             <p className='header__subtitle'>В данной работе представлено отображение данных в табличном формате с возможностью сортировки по запросу в поле Input</p>
         </div>
     );
